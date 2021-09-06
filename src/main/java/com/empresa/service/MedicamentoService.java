72419f91fabeb53e929996cca124c5e75fbbac9c
Rbhairava <renato.er@hotmail.com>
2021-09-05 19:19:13 -0500

    actvidad1

---
+++ src/main/java/com/empresa/service/MedicamentoService.java
@@ -0,0 +1,14 @@
+package com.empresa.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.empresa.entity.Medicamento;
+
+public interface MedicamentoService {
+	
+	public Medicamento insertaActualiza(Medicamento obj);
+	
+	public List<Medicamento> listaMedicamento();
+	
+}
